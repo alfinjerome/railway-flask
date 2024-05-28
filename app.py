5527c978e6c3a602147caac6f25228eb1d087a7e
Alfin Jerome <128155657+alfinjerome@users.noreply.github.com>
2024-05-28 10:12:08 +0000

    Added dynamic data as test

--- app.py
+++ app.py
@@ -1,10 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
-app=Flask(__name__)
+app = Flask(__name__)
+
+JOBS = [{
+    'id': 1,
+    'title': 'Data',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 200'
+}, {
+    'id': 2,
+    'title': 'BS',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 10,00,000'
+}, {
+    'id': 3,
+    'title': 'DS',
+    'location': 'UP, India',
+    'salary': 'Rs. 100,000'
+}]
+
 
 @app.route("/")
-def hello():
-    return render_template('home.html')
+def book():
+    return render_template('booking.html')
 
-if __name__=="__main__":
-  app.run(host="0.0.0.0",debug=True)
+
+@app.route("/listing")
+def list():
+    return render_template('trainlist.html', jobs=JOBS)
+
+
+@app.route('/api/listing')
+def listjson():
+    return jsonify(JOBS)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
